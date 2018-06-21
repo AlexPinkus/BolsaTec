c5335162fc2a686706a3e86179e2fa01716b4b84
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-06-21 13:52:38 -0500

    Actualizado app modules con los componentes, servicios y pupes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app.routes';
+
+// Services
+import { StudentService } from './services/student.service';
+
+// Components
 import { AppComponent } from './app.component';
+import { StudentComponent } from './components/student/student.component';
+import { StudentsComponent } from './components/students/students.component';
+
+// Pipes
+import { KeysPipe } from './pipes/keys.pipe';
+import { IndexComponent } from './components/index/index.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    StudentsComponent,
+    KeysPipe,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    StudentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
