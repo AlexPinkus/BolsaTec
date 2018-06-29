305d69ffe9b58889e11c6c212b6d1622692f0d7e
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-06-29 11:20:29 -0500

    App router cambiado por app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
 import { IndexComponent } from './components/index/index.component';
-import { StudentComponent } from './components/student/student.component';
+import { StudentRegisterComponent } from './components/student/studentregister.component';
+import { StudentProfileComponent } from './components/student/studentprofile.component';
 import { StudentsComponent } from './components/students/students.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
      { path: 'index', component: IndexComponent },
      { path: 'students', component: StudentsComponent },
-     { path: 'student/:id', component: StudentComponent },
+     { path: 'register/student', component: StudentRegisterComponent },
+     { path: 'profile/student/:id', component: StudentProfileComponent, canActivate: [AuthGuard] },
      { path: 'login', component: LoginComponent },
      { path: '**', pathMatch: 'full', redirectTo: 'index' },
 
