561af27958f2eff02da3abd1ee678e6ccd017ca6
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-07-24 10:20:44 -0500

    Otro del merge.

---
+++ src/app/components/empresa/empresaprofile/empresaprofile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empresaprofile',
+  templateUrl: './empresaprofile.component.html',
+  styleUrls: ['./empresaprofile.component.scss']
+})
+export class EmpresaprofileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
