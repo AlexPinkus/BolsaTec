e37d9e774c75f788bf9b007cb7850927ee2f58c4
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-08-03 16:21:51 -0500

    Correcciones a los valores default y funcionamiento de recuperar contraseña.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,22 +12,27 @@ import { User } from 'firebase';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  user = {
-    email: 'alexpinkus@hotmail.com',
-    password :  'password'
+  public user = {
+    email: '',
+    password:  ''
   };
 
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthService) { }
+    public authService: AuthService) { }
 
   ngOnInit() {
   }
 
   logIn() {
     // console.log( 'Usuario a logear:', this.user );
-    this.authService.login(this.user.email, this.user.password);
+    this.authService.login(this.user.email, this.user.password).then((credential) => {
+      // Logeo Exitoso:
+      // Hay que decidir a donde enviar al usuario...
+      this.router.navigate(['/inicio']);
+    }).catch((err) => {
+      console.log('Error al Logearse');
+    });
     // console.log('Usuario Loggeado:', this.authService.userDoc);
   }
 
