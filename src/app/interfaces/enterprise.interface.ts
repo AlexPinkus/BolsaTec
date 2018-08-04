dea45e86c749948231e5af71385682f197cbad15
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-08-03 21:08:07 -0500

    Limpieza de código.

--- src/app/interfaces/enterprise.interface.ts
+++ src/app/interfaces/enterprise.interface.ts
@@ -2,31 +2,31 @@ export interface Enterprise {
   uid?:      any;
   email:     string;
   // Datos contacto
-  firstName: string;
-  lastName:  string;
-  middleName?: string;
-  job?: string;
-  department?: string;
-  phone_contact:    number;
-  address_contact?: string;
+  firstName:    string;
+  lastName:     string;
+  middleName:   string;
+  job:          string;
+  department:   string;
+  contactPhone:   number;
+  contactAddress: string;
   // Datos empresa
-  comercialName: string;
-  bussinessName: string;
+  comercialName:  string;
+  bussinessName:  string;
   bussinessPhone: string;
-  bussinessTurn: string;
-  description: string;
-  RFC: string;
-  logo: string;
-  webURL?: string;
+  bussinessTurn:  string;
+  description:    string;
+  RFC:            string;
+  logo:           string;
+  webURL?:        string;
   address: {
-    mainStreet: string;
-    crossings:  string;
-    postalCode: number;
-    state:      string;
-    municipality?: string;
-    city?: string;
+    mainStreet:     string;
+    crossings:      string;
+    postalCode:     number;
+    city?:          string;
+    municipality?:  string;
+    state:          string;
   };
   createdOn?: number;
   isActive?:  boolean;
-  role?: string;
+  role?:      string;
 }
--- src/app/interfaces/student.interface.ts
+++ src/app/interfaces/student.interface.ts
@@ -1,63 +1,43 @@
-// export interface Student {
-//   uid?:      any;
-//   firstName: string;
-//   lastName:  string;
-//   email:     string;
-//   phone:     number;
-//   address: {
-//     mainStreet: string;
-//     crossings:  string;
-//     postalCode: number;
-//     state:      string;
-//   };
-//   // languages: {
-//   //   english: {
-//   //     written:  string;
-//   //     spoken:   string;
-//   //   }
-//   // };
-//   // createdOn: Date;
-//   // isActive:  boolean;
-// }
 export interface Student {
   uid?:      any;
   // Matrícula
   idStudent?: any;
-  firstName: string;
-  lastName:  string;
-  middleName?: string;
-  age?: Date;
-  sex?: string;
-  email:     string;
-  phone:     number;
+  firstName:  string;
+  middleName: string;
+  lastName:   string;
+  age:        Date;
+  sex:        string;
+  email:      string;
+  phone:      number;
+
   address: {
-    mainStreet: string;
-    crossings:  string;
-    postalCode: number;
-    state:      string;
+    mainStreet:    string;
+    crossings:     string;
+    postalCode:    number;
+    state:         string;
     neighborhood?: string;
     municipality?: string;
-    city?: string;
+    city?:         string;
   };
   languages?: {
     english: {
-      written:  string;
-      spoken:   string;
+      written:     string;
+      spoken:      string;
       translation: string;
     }
   };
   degree?: {
-    bachelor: string;
+    bachelor:   string;
     speciality: string;
-    master: string;
-    phd: string;
+    master:     string;
+    phd:        string;
   };
-  experience?: string;
-  resumeURL?:  string;
-  speciality?: string;
+  experience?:    string;
+  resumeURL?:     string;
+  speciality?:    string;
   maritalStatus?: string;
-  createdOn?: Date;
   isGraduated?: boolean;
+  createdOn?: number;
   isActive?:  boolean;
-  role?: string;
+  role?:      string;
 }
