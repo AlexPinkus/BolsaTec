533a70b591b482445941c3f83eafddb29e8713c9
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-08-27 20:13:46 -0500

    Funcionalidad JobOffer

--- src/app/components/joboffer/joboffer-main/joboffer-main.component.ts
+++ src/app/components/joboffer/joboffer-main/joboffer-main.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Joboffer } from '../../../interfaces/joboffer.interface';
+import { JobofferService } from '../../../services/joboffer.service';
+import { EnterpriseService } from '../../../services/enterprise.service';
+
+import { Observable } from 'rxjs';
+import { map, take, tap, finalize, switchMap, flatMap } from 'rxjs/operators';
+import { combineLatest } from 'rxjs/observable/combineLatest';
 
 @Component({
   selector: 'app-joboffer-main',
@@ -6,6 +13,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joboffer-main.component.scss']
 })
 export class JobofferMainComponent implements OnInit {
+
+  public jobOffers$: Observable<any>;
+
   public bachelors = [
     'Licenciatura en Ingeniería Industrial',
     'Licenciatura en Ingeniería Bioquímica',
@@ -22,16 +32,50 @@ export class JobofferMainComponent implements OnInit {
     'Licenciatura en Administración en Educación a Distancia'
   ];
 
+  public selectedBachelor: string;
+  public searchFilter: string;
+
   public item = [
 
   ]
-  constructor() { }
+
+
+
+  constructor(private jobofferService: JobofferService,
+  private enterpriseService: EnterpriseService) {
+    this.jobOffers$ = this.jobofferService.joboffersCollection.valueChanges().pipe(
+      map(jobOffers => {
+        return jobOffers.map(jobOffer => {
+          return this.enterpriseService.getEnterprise(jobOffer.idEnterprise).valueChanges().pipe(
+            map(enterprise => Object.assign({}, {enterpriseLogo: enterprise.logo, enterpriseName: enterprise.comercialName, ...jobOffer}))
+          );
+        });
+      }),
+      flatMap(observables => combineLatest(observables))
+    );
+  }
 
   ngOnInit() {
   }
 
-  updateFilter(event) {
-
+  updateFilter(jobOffer) {
+    // console.log('event :', event);
+    // console.log('searchFilter :', this.searchFilter);
+    if (this.searchFilter) {
+      return ( jobOffer.enterpriseName.includes(this.searchFilter) ||
+      jobOffer.position.includes(this.searchFilter))
+    } else {
+      return true;
+    }
   }
 
+  filter(bachelors: string[]) {
+    if (this.selectedBachelor) {
+      console.log('selectedBachelor :', this.selectedBachelor);
+      return bachelors.includes(this.selectedBachelor);
+    } else {
+      return true;
+    }
+
+  }
 }
