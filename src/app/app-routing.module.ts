6dcdc97409fa5187ccbec116c09d31f3aa7a87ee
José Alejandro Castillo Pinkus <jose.castillo@springlabs.net>
2018-09-05 16:18:18 -0500

    Estructuración de las rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IndexComponent } from './components/index/index.component';
+// Guards
 import { AuthGuard, NotAuthGuard } from './services/auth.guard';
 import { ProfileGuard } from './services/profile.guard';
 import { StudentGuard, NotStudentGuard } from './services/student.guard';
-import { IndexComponent } from './components/index/index.component';
 // Componentes de Students
 import { StudentRegisterComponent } from './components/student/student-register/student-register.component';
 import { StudentProfileComponent } from './components/student/student-profile/student-profile.component';
@@ -12,16 +13,18 @@ import { StudentsComponent } from './components/students/students.component';
 // Componentes de Enterprises
 import { EnterpriseRegisterComponent } from './components/enterprise/enterprise-register/enterprise-register.component';
 import { EnterpriseProfileComponent } from './components/enterprise/enterprise-profile/enterprise-profile.component';
-
+// Componentes de JobOffers
 import { JobofferRegisterComponent } from './components/joboffer/joboffer-register/joboffer-register.component';
+import { JobofferViewComponent } from './components/joboffer/joboffer-view/joboffer-view.component';
+import { JobofferMainComponent } from './components/joboffer/joboffer-main/joboffer-main.component';
 import { JobofferListComponent } from './components/joboffer/joboffer-list/joboffer-list.component';
+// Componentes de Admin
+import { StudentsAdminComponent } from './components/administrador/students-admin/students-admin.component';
+import { EnterprisesAdminComponent } from './components/administrador/enterprises-admin/enterprises-admin.component';
 
 
 import { LoginComponent } from './components/login/login.component';
 
-import { StudentsAdminComponent } from './components/administrador/students-admin/students-admin.component';
-import { JobofferViewComponent } from './components/joboffer/joboffer-view/joboffer-view.component';
-import { JobofferMainComponent } from './components/joboffer/joboffer-main/joboffer-main.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
 
 const routes: Routes = [
@@ -46,6 +49,7 @@ const routes: Routes = [
     { path: 'joboffer/:id',  component: JobofferViewComponent, canActivate: [AuthGuard]  },
 
     { path: 'admin', component: StudentsAdminComponent },
+    { path: 'admin/enterprises', component: EnterprisesAdminComponent },
     { path: '**', component: NotfoundComponent },
 
     // { path: 'path/:routeParam', component: MyComponent },
